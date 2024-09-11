8c48766809cd1d0e21ff420108d8ca28d2a2af5c
mohanadtayeb <38011329+mohanadtayeb@users.noreply.github.com>
2024-09-11 19:23:05 +0300

    add a location request

--- src/components/MainSection.svelte
+++ src/components/MainSection.svelte
@@ -10,12 +10,31 @@
   let weekday;
   let prayerTimes;
   let nameCity;
+  let locationPermissionGranted = false;
 
   $: ({ lat, lng, loading, error } = $geolocation);
-  $: if (lat && lng && !loading && !error) {
+  $: if (lat && lng && !loading && !error && locationPermissionGranted) {
     fetchPrayerTimes(lat, lng);
     fetchCityName(lat, lng);
   }
+
+  const requestLocationPermission = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        () => {
+          locationPermissionGranted = true;
+          geolocation.start();
+        },
+        (error) => {
+          console.error("Error requesting location permission:", error);
+          locationPermissionGranted = false;
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+      locationPermissionGranted = false;
+    }
+  };
 
   const fetchPrayerTimes = async (latitude, longitude) => {
     try {
@@ -46,26 +65,40 @@
       console.error("Error fetching name city:", error);
     }
   };
+
+  onMount(() => {
+    requestLocationPermission();
+  });
 </script>
 
 <section
   class="font-['Cairo'] h-[55rem] flex items-center justify-center text-center bg-gradient-to-r from-[#232F3D] to-[#3D4957]"
 >
   <article class="mx-4">
-    <h1 class=" font-bold mb-4 text-white max-sm:text-7xl text-8xl py-4">
+    <h1 class="font-bold mb-4 text-white max-sm:text-7xl text-8xl py-4">
       أذان
     </h1>
-    <p class=" mb-6 text-white">
+    <p class="mb-6 text-white">
       بناء على موقعك الجغرافي سيتم تحديد اوقات الصلاة
     </p>
+    {#if !locationPermissionGranted}
+      <button
+        on:click={requestLocationPermission}
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        السماح بالوصول إلى الموقع
+      </button>
+    {/if}
   </article>
 </section>
 
-<PrayerTable
-  {gregorianDate}
-  {hijri}
-  {hijriMonth}
-  {weekday}
-  {prayerTimes}
-  {nameCity}
-/>
+{#if locationPermissionGranted && prayerTimes}
+  <PrayerTable
+    {gregorianDate}
+    {hijri}
+    {hijriMonth}
+    {weekday}
+    {prayerTimes}
+    {nameCity}
+  />
+{/if}
